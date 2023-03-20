91eb21a85d29f023731abfebd8e7d9e6c4e40464
Clebson Pessoa <cvafam@gmail.com>
2023-03-20 18:16:28 -0300

    subindo estrutura inicial

---
+++ src/components/Layout/index.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <aside>
+            Barra Lateral
+        </aside>
+        <div>
+            <header>
+                Cabeçalho do Sistema
+            </header>
+            <main>
+                Conteudo Principal
+            </main>
+            <footer>
+                Rodapé da Página
+            </footer>
+        </div>
+    </div>
+</template>
+
+<script lang='ts'>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    name: 'Template'
+})
+</script>
+
+
+<style scoped>
+
+</style>
